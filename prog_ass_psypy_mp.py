d666d873fd3431a067a9b9f9d46bb1df9afaac40
MaxHe-Ho <maxheho@gmail.com>
2020-03-07 20:17:16 +0100

    Added comment to quit function.

--- prog_ass_psypy_mp.py
+++ prog_ass_psypy_mp.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Subject plays a matching pennies game: 
+Subject plays a matching pennies game:
     One player = 'even', other player = 'odd', each has a penny.
     Player and computer present their penny heads-up or tails-up. If the two pennies match, the 'even' player wins a point and if they don't, the 'odd' player wins.
-    
+
 By pressing 'h', the subject chooses heads and by pressing 't', the subject chooses tails. The computer chooses randomly. After the subject has made their choice, both pennies
 are shown on the screen and subject gets the information whether they won and what the current scores are. The subject can quit at any point by pressing a quit key.
-Additional features: 
+Additional features:
     - Computer with a different strategy than 'random choice' --> bias in direction of heads or tails or switching from previous round (or different response
-      than player beforehands, frustrator). 
+      than player beforehands, frustrator).
     - printout at the end of the program showing how often the subject switched their choice from own & computers choice in previous round.
-    
+
 User experience really important!
 """
 
@@ -28,7 +28,7 @@ You will play matching pennies against the computer.
 Press any key to begin.
 """
 
-instruction_text = """You are the "even" player, i.e. you win a given round when the amount of heads (and tails) 
+instruction_text = """You are the "even" player, i.e. you win a given round when the amount of heads (and tails)
 is even.
 Press 'h' for head and 't' for tails.
 To exit, press q.
@@ -71,10 +71,10 @@ core.wait(4)
 # randomize choice of robot
 
 #choice_subject
-# if choice_subject == choice of robot: 
+# if choice_subject == choice of robot:
     # Visual.TextStim (win, text = "you win")
 
-# %% 
+# %%
 
 
 """
@@ -85,12 +85,11 @@ core.wait(4)
 
 event.globalKeys.add(key='q', func=core.quit)
 
+# check on psychopy website: endless loop so window never gets closed
 while True:
     instruction.draw()
     win.flip()
 
 
 """
-win.close() 
-
-
+win.close()
